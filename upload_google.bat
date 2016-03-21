0a222ef9e1dcc76faef08e0529ffe395541044c6
gaurav sh <gaurav2908@gmail.com>
2016-03-20 19:55:13 -0800

    build

--- upload_google.bat
+++ upload_google.bat
@@ -1,4 +1,4 @@
 rem put this bat file in tomcat webapps folder i.e. "C:\Jyoti\apache-tomcat-7.0.65\webapps", extract EzSearch.war file if not already extracted then execute in command prompt
 
-appcfg.cmd -A ezsearch-1210 update EzSearch
+appcfg.cmd -A ezsearch-project update EzSearch
 
